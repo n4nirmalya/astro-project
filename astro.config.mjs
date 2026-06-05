3e0ad41453c03832c13494afa60eb478cf2d698d
Nirmalya Pratap Dagar <nirmalyapratap.np17@gmail.com>
2026-06-05 20:10:22 +0530

    added docs folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   base: '/astro-project',
+  outDir: './docs',
   integrations: [
     tailwind({
       // We will define custom styles in global.css rather than injecting theme-level default base
